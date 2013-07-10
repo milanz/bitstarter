3cfe16dc7eb1b67c03e805f6c7e0eec2af9060b5
Milan Zivkovic <milan.zivkovic@gmail.com>
2013-07-10 23:57:38 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var data = fs.readFileSync('index.html','utf8');
+var data = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(data);
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
