857f2f275ac506d47302ee2ee793e6e5ab6fa33b
Milan Zivkovic <milan.zivkovic@gmail.com>
2013-07-10 23:48:54 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
