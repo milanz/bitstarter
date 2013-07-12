f1b29c41cf3361d1213a0285fdaa6107276239f7
Milan Zivkovic <milan.zivkovic@gmail.com>
2013-07-12 11:34:58 +0000

    crowdfunding site

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var fs = require('fs');
-#
+
 var app = express.createServer(express.logger());
 
 #var data = new Buffer(100);
-var data = fs.readFileSync('index.html');
+var data = fs.readFileSync('index2.html');
 
 app.get('/', function(request, response) {
   response.send(data.toString());
